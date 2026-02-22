1f6a9288d5789967e64dfa9d0593ce8b087c2224
mariem-lamine <mariem.lamine@esprit.tn>
2026-02-22 21:36:32 +0100

    Routage complet

--- src/app/features/suggestions/list-suggestion/list-suggestion.component.ts
+++ src/app/features/suggestions/list-suggestion/list-suggestion.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Suggestion } from '../../models/suggestion';
+import { Suggestion } from '../../../models/suggestion';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-list-suggestion',
